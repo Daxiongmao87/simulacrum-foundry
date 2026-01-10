f256d7031c1a8f6c53a0967a5de72841b2e50fe3
Daxiongmao87 <daxiongmao87@gmail.com>
2026-01-09 21:57:35 -0600

    style: subdue header scaling in chat messages

--- scripts/build-info.js
+++ scripts/build-info.js
@@ -1,3 +1,3 @@
 // Auto-generated build info - DO NOT EDIT MANUALLY
-export const BUILD_HASH = 'a589035c';
-export const BUILD_TIME = '2026-01-10T03:30:33Z';
+export const BUILD_HASH = 'f613c312';
+export const BUILD_TIME = '2026-01-10T03:51:22Z';
