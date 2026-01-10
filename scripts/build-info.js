fe75f3219ed441c342c77e16b969b809201c1edf
Daxiongmao87 <daxiongmao87@gmail.com>
2026-01-09 20:47:47 -0600

    chore: update build info

--- scripts/build-info.js
+++ scripts/build-info.js
@@ -1,3 +1,3 @@
 // Auto-generated build info - DO NOT EDIT MANUALLY
-export const BUILD_HASH = '8070f553';
-export const BUILD_TIME = '2026-01-09T21:03:44Z';
+export const BUILD_HASH = '95e339c5';
+export const BUILD_TIME = '2026-01-10T02:36:44Z';
